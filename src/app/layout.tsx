236c7840fabb983acaad71c1d3974d4d24f4a18e
YannOuafete <yannouafete@gmail.com>
2025-03-13 16:24:51 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Menu from "./menu"
+import Menu from "./components/menu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
