32e9b018802045209a14fadd036ebe06bec8325e
YannOuafete <yannouafete@gmail.com>
2025-03-17 11:14:29 +0100

    finition de Contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,11 +5,11 @@ import { FaFacebookF,FaInstagram, FaTwitter } from "react-icons/fa";
 
 export default function About() {
   return (
-    <div className="m-10 mt-70 flex flex-col gap-40">
-       <div className="flex  ">
-          <div className=" flex flex-col w-1/2  font-serif  gap-10">
+    <div className="mt-80 flex flex-col mb-90 p-9 ">
+       <div className="flex  gap-60">
+          <div className=" flex flex-col w-1/2  font-serif mt-4 gap-10 ">
             <div className="text-5xl">Contact Us</div>
-            <div className="text-2xl font-serif leading-10">If you have any questions or comments, please contact us via email<br/>or phone, or send us a message using the contact form.</div>
+            <div className="text-2xl font-serif">If you have any questions or comments, please contact us via email or phone, or send us a message using the contact form.</div>
              <div className="flex flex-col gap-2" >
                <p className="text-2xl font-serif font-bold">  Email</p>
                <p className="text-xl">yannouafete@gmail.com</p>
@@ -27,23 +27,52 @@ export default function About() {
              
            </div>
 
-          <div className="w-1/2 flex flex-col gap-4 text-3xl font-serif border ">
-            <div className="leading-10">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident,sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div>
-        
-           <div className="leading-10">
-          
-               Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-               laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto 
-               beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia 
-               consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, 
-               adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam.
-            </div>
+          <div className="w-2/3 flex flex-col gap-4 text-2xl text-gray-500 font-serif  border-red-500 p-3  ">
+             <form action="" className="flex flex-col   gap-5 p-2">
+              <div className="flex flex-col gap-3">
+                <div>Name (required)</div>
+                <div className="flex gap-2 ">
+                    <div className="flex flex-col gap-2 w-1/2 ">
+                       <label htmlFor="FirstName">First Name (required)</label>
+                       <input type="text" 
+                             className=" bg-[#f1f1ef] p-3  rounded-md"
+                             required />
+                    </div>
+                    <div className="flex flex-col w-1/2 gap-2 ">
+                      <label htmlFor="LastName">Last Name (required)</label>
+                      <input type="text" 
+                             className="  bg-[#f1f1ef] p-3  rounded-md"
+                             required />
+                    </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="email">Email (required)</label>
+                <input type="email"
+                       className="bg-[#f1f1ef] p-3 rounded-md"
+                       required />
+              </div>
+              <div className="flex flex-col gap-2">
+                  <label htmlFor="phone">Phone (required)</label>
+                  <input type="phone" 
+                         className=" bg-[#f1f1ef] p-3  rounded-md"
+                         required/>
+              </div>
+              <div className="flex flex-col gap-2">
+                  <label htmlFor="subject">Subject (required)</label>
+                  <input type="text" 
+                         className="  bg-[#f1f1ef] p-3  rounded-md" 
+                         required/>
+              </div>
+              <div className="flex flex-col gap-2">
+                  <label htmlFor="message">Message (required)</label>
+                  <textarea name="message" id=""  className=" bg-[#f1f1ef] h-40 rounded-md"></textarea>
+              </div>
+              <button className="bg-black text-white  w-fit text-2xl pt-6 pb-6 pl-12 pr-12 rounded-xl">
+                    Submit
+               </button>
+              
+             </form>
           </div>
        </div> 
     </div>
